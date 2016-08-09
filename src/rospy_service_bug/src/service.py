e7788357e708db5fdce3150b1e2f04b97560cc43
Eric Nelson <enelson@morse-corp.com>
2016-08-09 15:52:58 -0400

    Simplified a bit more

--- src/rospy_service_bug/src/service.py
+++ src/rospy_service_bug/src/service.py
@@ -5,16 +5,22 @@ from rospy_service_bug.srv import *
 from rospy_service_bug.msg import *
 
 
-def service_1(service_info):
+def service_response(service_info):
     print service_info
     retval = CustomMessage
     retval.custom_field = "Custom message that is important"
     return retval
 
+def service_request(service_info):
+    print service_info
+    return "benign response"
+
 
 def main():
     rospy.init_node("tupac_system_performance_profiler")
-    service_one = rospy.Service('service_1', Service1, service_1)
+    _service_echo = rospy.Service('service_echo', ServiceEcho, service_response)
+    _service_response = rospy.Service('service_response', ServiceResponse, service_response)
+    _service_request = rospy.Service('service_request', ServiceRequest, service_request)
     rospy.spin()
 
 if __name__ == '__main__':
--- src/rospy_service_bug/src/service_proxy.py
+++ src/rospy_service_bug/src/service_proxy.py
@@ -8,18 +8,35 @@ from rospy_service_bug.msg import *
 class PoorlyNamedClass:
     def __init__(self):
         rospy.init_node("service_proxy_tester")
-        rospy.wait_for_service('service_1')
-        self._service_1 = rospy.ServiceProxy('service_1', Service1)
+        rospy.wait_for_service('service_echo')
+        rospy.wait_for_service('service_response')
+        rospy.wait_for_service('service_request')
+        self._service_echo = rospy.ServiceProxy('service_echo', ServiceEcho)
+        self._service_response = rospy.ServiceProxy('service_response', ServiceResponse)
+        self._service_request = rospy.ServiceProxy('service_request', ServiceRequest)
 
         self._debug_count = 0
 
-    def one(self):
+    def echo(self):
         self._debug_count += 1
-        print '{} One'.format(self._debug_count)
-        # Inserting "BREAKING CODE" here will cause the problem
+        print '{} Echo'.format(self._debug_count)
         important = CustomMessage
         important .custom_field = "Custom message that is important"
-        retval = self._service_1(important )
+        retval = self._service_echo(important)
+        print retval
+
+    def request(self):
+        self._debug_count += 1
+        print '{} Request'.format(self._debug_count)
+        important = CustomMessage
+        important .custom_field = "Custom message that is important"
+        retval = self._service_request(important)
+        print retval
+
+    def response(self):
+        self._debug_count += 1
+        print '{} Response'.format(self._debug_count)
+        retval = self._service_response("benign request")
         print retval
 
 def main():
@@ -32,25 +49,14 @@ def main():
     #foo = CustomMessage
     #foo.custom_field = "Custom message on the callstack that should be ignored"
 
-    if do_first:
-        for _ in xrange(3):
-            bn.one()
+    #for _ in xrange(3):
+    #    bn.echo()
 
-        for _ in xrange(3):
-            bn.two()
+    #for _ in xrange(3):
+    #    bn.request()
 
-        for _ in xrange(3):
-            bn.one()
-
-    if not do_first:
-        for _ in xrange(3):
-            bn.two()
-
-        for _ in xrange(3):
-            bn.one()
-
-        for _ in xrange(3):
-            bn.two()
+    for _ in xrange(3):
+        bn.response()
 
 
 if __name__ == '__main__':
