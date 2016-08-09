f4464e02a94ed64ea452249b3e23402cedf38e5e
Eric Nelson <enelson@morse-corp.com>
2016-08-09 15:19:57 -0400

    Added more comments

--- src/rospy_service_bug/src/service_proxy.py
+++ src/rospy_service_bug/src/service_proxy.py
@@ -5,7 +5,7 @@ from rospy_service_bug.srv import *
 from rospy_service_bug.msg import *
 
 
-class BadName:
+class PoorlyNamedClass:
     def __init__(self):
         rospy.init_node("service_proxy_tester")
         rospy.wait_for_service('service_1')
@@ -18,26 +18,27 @@ class BadName:
     def one(self):
         self._debug_count += 1
         print '{} One'.format(self._debug_count)
-
+        # Inserting "BREAKING CODE" here will cause the problem
         retval = self._service_1(self._debug_count)
         print retval
 
     def two(self):
         self._debug_count += 1
         print '{} Two'.format(self._debug_count)
-
+        # Inserting "BREAKING CODE" here will cause the problem
         retval = self._service_2(self._debug_count)
         print retval
 
 
 def main():
-    bn = BadName()
+    bn = PoorlyNamedClass()
 
     do_first = True
 
-    # The following two lines break it
-    retval = CustomMessage
-    retval.custom_field = "Custom message on the callstack that should be ignored"
+    # Inserting "BREAKING CODE" here will cause the problem
+    # The following two lines break it, and are called "BREAKING CODE"
+    foo = CustomMessage
+    foo.custom_field = "Custom message on the callstack that should be ignored"
 
     if do_first:
         for _ in xrange(3):
